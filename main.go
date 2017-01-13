e127d145ce14a77059cfc8813d5caec5e31c246c
Jeremy Cloutier <jcloutier24@yahoo.com>
2017-01-12 19:38:50 -0500

    multiple updates and added tests

---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type key int
+
+const (
+	timestampType key = iota
+)
+
+// Timestamp represents the timestamp returned to the user
+type Timestamp struct {
+	Unix    *int64  `json:"unix"`
+	Natural *string `json:"natural"`
+}
+
+func jsonMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		h(w, r)
+	}
+}
+
+func dateParserMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Get date from route parameter
+		vars := mux.Vars(r)
+		timeArg := vars["date"]
+
+		// Declare t to hold return
+		var t Timestamp
+		var e error
+
+		// check for integer value
+		if tInt, err := strconv.ParseInt(timeArg, 10, 64); err == nil {
+			// if integer parse timestamp
+			t, e = parseTimestamp(tInt)
+		} else {
+			// if string parse string
+			t, e = parseTimestring(timeArg)
+		}
+
+		// If either parse function returned an error, call the error handler and
+		// return early
+		if e != nil {
+			errorHandler(w, r)
+			return
+		}
+
+		// create new context with Timestamp
+		ctx := context.WithValue(r.Context(), timestampType, &t)
+		r = r.WithContext(ctx)
+
+		h(w, r)
+	}
+}
+
+func app() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", index)
+	r.HandleFunc("/{date:[0-9]+}", jsonMiddleware(dateParserMiddleware(unixTimeStamp)))
+	r.HandleFunc("/{date}", jsonMiddleware(dateParserMiddleware(naturalTimeStamp)))
+
+	return r
+}
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "localhost:8080"
+	} else {
+		port = ":" + port
+	}
+	fmt.Println("Server listening on port: " + port)
+	if err := http.ListenAndServe(port, app()); err != nil {
+		log.Println(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	path := path.Join("index.html")
+
+	temp, _ := template.ParseFiles(path)
+	temp.Execute(w, nil)
+}
+
+// unixTimeStamp will handle a request to to process a unix style integer timestamp
+func unixTimeStamp(w http.ResponseWriter, r *http.Request) {
+	t := r.Context().Value(timestampType).(*Timestamp)
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(*t); err != nil {
+		log.Println(err)
+	}
+}
+
+// naturalTimeStamp will process a human readable timestamp in the format
+// January 2, 2017
+func naturalTimeStamp(w http.ResponseWriter, r *http.Request) {
+	t := r.Context().Value(timestampType).(*Timestamp)
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(*t); err != nil {
+		log.Println(err)
+	}
+}
+
+// errorHandler handles responses to bad requests to the api endpoints.
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-Status-Reason", "Invalid date time format, refer to documentation.")
+	w.WriteHeader(http.StatusBadRequest)
+	if err := json.NewEncoder(w).Encode(Timestamp{}); err != nil {
+		log.Println(err)
+	}
+}
+
+// parseTimestamp will parse a unix timestamp into a Timestamp struct to return
+// as json
+func parseTimestamp(timestamp int64) (Timestamp, error) {
+	if timestamp < 0 {
+		return Timestamp{}, errors.New("Invalid Timestamp: less than 0")
+	}
+	t := time.Unix(timestamp, 0)
+	unix, natural := t.Unix(), t.UTC().String()
+	return Timestamp{&unix, &natural}, nil
+}
+
+// parseTimestring will parse a human readable date format matching the specified
+// layout into a Timestamp struct
+func parseTimestring(timestamp string) (Timestamp, error) {
+	layout := "January _2, 2006"
+	t, err := time.Parse(layout, timestamp)
+	if err != nil {
+		return Timestamp{nil, nil}, err
+	}
+	unix, natural := t.Unix(), t.UTC().String()
+	return Timestamp{&unix, &natural}, nil
+}
---
+++ main_test.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestEncodeEmptyTimestamp(t *testing.T) {
+	var bb bytes.Buffer
+
+	if err := json.NewEncoder(&bb).Encode(Timestamp{}); err != nil {
+		t.Fatal(err)
+	}
+
+	got := strings.TrimSpace(bb.String())
+	want := `{"unix":null,"natural":null}`
+
+	if got != want {
+		t.Log("Wanted   :", want)
+		t.Log("Got      :", got)
+		t.Fatal("Mismatch")
+	}
+}
+
+func TestEncodeTimestamp(t *testing.T) {
+	var bb bytes.Buffer
+	var unix int64 = 1450137600
+	natural := `2015-12-15 00:00:00 +0000 UTC`
+	timestamp := Timestamp{&unix, &natural}
+	if err := json.NewEncoder(&bb).Encode(timestamp); err != nil {
+		t.Fatal(err)
+	}
+
+	got := strings.TrimSpace(bb.String())
+	want := `{"unix":1450137600,"natural":"2015-12-15 00:00:00 +0000 UTC"}`
+
+	if got != want {
+		t.Log("Wanted  :", want)
+		t.Log("Got     :", got)
+		t.Fatal("Mismatch")
+	}
+}
+
+func TestParseTimestamp(t *testing.T) {
+	var unix int64 = 1450137600
+
+	got, err := parseTimestamp(unix)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var wantUnix int64 = 1450137600
+	wantNat := `2015-12-15 00:00:00 +0000 UTC`
+	if (*got.Unix != wantUnix) || (*got.Natural != wantNat) {
+		t.Log("Wanted   :", wantUnix, wantNat)
+		t.Log("Got      :", *got.Unix, *got.Natural)
+
+		t.Fatal("Mismatch")
+	}
+}
+
+func TestParseInvalidTimestamp(t *testing.T) {
+	var unix int64 = -1
+
+	timestamp, got := parseTimestamp(unix)
+	if !reflect.DeepEqual(timestamp, Timestamp{}) {
+		t.Fatal("Returned Timestamp should be empty: Timestamp{}")
+	}
+
+	want := `Invalid Timestamp: less than 0`
+	if got.Error() != want {
+		t.Log("Wanted   :", want)
+		t.Log("Got      :", got)
+
+		t.Fatal("Mismatch")
+	}
+}
+
+func TestParseTimestring(t *testing.T) {
+	timestring := `December 15, 2015`
+
+	got, err := parseTimestring(timestring)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var wantUnix int64 = 1450137600
+	wantNat := `2015-12-15 00:00:00 +0000 UTC`
+	if (*got.Unix != wantUnix) || (*got.Natural != wantNat) {
+		t.Log("Wanted   :", wantUnix, wantNat)
+		t.Log("Got      :", *got.Unix, *got.Natural)
+
+		t.Fatal("Mismatch")
+	}
+}
+
+func TestParseInvalidTimestring(t *testing.T) {
+	timestring := `Dec 15, 2015`
+
+	timestamp, got := parseTimestring(timestring)
+
+	if !reflect.DeepEqual(timestamp, Timestamp{}) {
+		t.Fatal("Returned Timestamp should be empty: Timestamp{}")
+	}
+
+	if !strings.Contains(got.Error(), `cannot parse`) {
+		t.Fatal("Invalid date should be be parsed")
+	}
+}
+
+func TestIndexRoute(t *testing.T) {
+	ts := httptest.NewServer(app())
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := `Timestamp Microservice`
+	got := string(b)
+
+	if !strings.Contains(got, want) {
+		t.Log("Wanted:", want)
+		t.Fatal("Incorrect Title")
+	}
+}
+
+func TestTimestampRouteHeader(t *testing.T) {
+	ts := httptest.NewServer(app())
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + `/1450137600`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := res.Header.Get(`Content-Type`)
+	want := `application/json; charset=UTF-8`
+	if got != want {
+		t.Log("Invalid Content-Type Header")
+		t.Log("Wanted   :", want)
+		t.Log("Got      :", got)
+		t.Fatal("Mismatch")
+	}
+}
+
+func TestTimestampRoute(t *testing.T) {
+	ts := httptest.NewServer(app())
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + `/1450137600`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := `{"unix":1450137600,"natural":"2015-12-15 00:00:00 +0000 UTC"}`
+	got := strings.TrimSpace(string(b))
+
+	if got != want {
+		t.Log("Wanted   :", want)
+		t.Log("Got      :", got)
+		t.Fatal("Mismatch")
+	}
+}
